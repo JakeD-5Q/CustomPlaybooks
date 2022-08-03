c63a0646c25e23f4a660e4d5bdcc28cadab28343
Jake Derkowski <77984636+JakeD-5Q@users.noreply.github.com>
2022-08-02 20:56:11 -0500

    deploy scripts

--- Initiate-MDEInvestigation/deploy.ps1
+++ Initiate-MDEInvestigation/deploy.ps1
@@ -8,8 +8,8 @@ $suffix = Get-Random -Maximum 100
 $deploySuffix = $today + "_$suffix"
 
 # remote deploys
-$deploymentName = "" + $deploySuffix
-$remoteUrl = ""
+$deploymentName = "Initiate-MDEInvestigation" + $deploySuffix
+$remoteUrl = "https://raw.githubusercontent.com/JakeD-5Q/CustomPlaybooks/main/Initiate-MDEInvestigation/azuredeploy.json"
 $localTemplate = 'enter.local.file.json'
 New-AzResourceGroupDeployment -Name $deploymentName `
     -ResourceGroupName $ResourceGroup `
